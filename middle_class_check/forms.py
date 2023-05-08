7c345045eeaccadbb9cb9b150e05c21f4f663c07
anesta95 <adriannesta@gmail.com>
2023-05-07 22:26:57 -0400

    creating django form

---
+++ middle_class_check/forms.py
@@ -0,0 +1,47 @@
+from django import forms
+from dynamic_forms import DynamicField, DynamicFormMixin
+from .models import MedianHouseholdIncome
+
+class AreaForm(DynamicFormMixin, forms.Form):
+
+    def geo_choices(form):
+        year = form['year'].value()
+        state_name = form['state'].value()
+        geography_name = form['area'].value()
+        return MedianHouseholdIncome.objects.filter(year=year).filter(state_name=state_name).filter(geography_name=geography_name).distinct()
+
+    def initial_geo(form):
+        year = form['year'].value()
+        state_name = form['state'].value()
+        geography_name = form['area'].value()
+        return MedianHouseholdIncome.objects.filter(year=year).filter(state_name=state_name).filter(geography_name=geography_name).distinct().first()
+
+    # Year field
+    year = forms.ModelChoiceField(
+        queryset=MedianHouseholdIncome.objects.values_list('year', flat=True).distinct().order_by('-year'),
+        initial=MedianHouseholdIncome.objects.values_list('year', flat=True).distinct().order_by('-year').first()
+    )
+
+    # State field
+    state = forms.ModelChoiceField(
+        queryset=MedianHouseholdIncome.objects.exclude(state_name = 'United States').values_list('state_name', flat=True).distinct(),
+        initial=MedianHouseholdIncome.objects.exclude(state_name = 'United States').values_list('state_name', flat=True).distinct().first()
+    )
+
+    # Areas field
+    area = forms.ModelChoiceField(
+        queryset=MedianHouseholdIncome.objects.exclude(geography_name = 'national').values_list('geography_name', flat=True).distinct(),
+        initial=MedianHouseholdIncome.objects.exclude(geography_name = 'national').values_list('geography_name', flat=True).distinct().first()
+    )
+
+    # Geo dynamic field
+    geos = DynamicField(
+        forms.ModelChoiceField,
+        queryset=geo_choices,
+        initial=initial_geo
+    )
+
+    income = forms.IntegerField(
+        min_value = 0,
+        max_value = 100000000
+    )
--- middle_class_check/views.py
+++ middle_class_check/views.py
@@ -1,32 +1,36 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import MedianHouseholdIncome
+from .forms import AreaForm
 
 # Create your views here.
 def index(request):
-    """View function for home page of site."""
-    # Generate unique list of years, states, and area options
-    years = MedianHouseholdIncome.objects.values_list('year', flat=True).distinct().order_by('-year')
-    states = MedianHouseholdIncome.objects.exclude(state_name = 'United States').values_list('state_name', flat=True).distinct()
-    areas = MedianHouseholdIncome.objects.exclude(geography_name = 'national').values_list('geography_name', flat=True).distinct()
-    
-    context = {
-        'unique_years': years,
-        'unique_states': states,
-        'unique_areas': areas,
-    }
-
-    # Render the HTML template index.html with the data in the context variable.
+    form = AreaForm()
+    context = {'form': form}
     return render(request, 'index.html', context=context)
 
+# def index(request):
+#     """View function for home page of site."""
+#     # Generate unique list of years, states, and area options
+#     years = MedianHouseholdIncome.objects.values_list('year', flat=True).distinct().order_by('-year')
+#     states = MedianHouseholdIncome.objects.exclude(state_name = 'United States').values_list('state_name', flat=True).distinct()
+#     areas = MedianHouseholdIncome.objects.exclude(geography_name = 'national').values_list('geography_name', flat=True).distinct()
+    
+#     context = {
+#         'unique_years': years,
+#         'unique_states': states,
+#         'unique_areas': areas,
+#     }
+
+#     # Render the HTML template index.html with the data in the context variable.
+#     return render(request, 'index.html', context=context)
+
 def locales(request):
-    year = request.GET.get('year')
-    state_name = request.GET.get('state_name')
-    geography_name = request.GET.get('geography_name')
-    print(year)
-    print(state_name)
-    print(geography_name)
-    locales = MedianHouseholdIncome.objects.filter(year=year).filter(state_name=state_name).filter(geography_name=geography_name).distinct()
-    context = {'locales': locales}
-    print(locales)
-    return render(request, "partials/locales.html", context=context)
+    form = AreaForm(request.GET)
+    return HttpResponse(form['geos'])
+    # year = request.GET.get('year')
+    # state_name = request.GET.get('state_name')
+    # geography_name = request.GET.get('geography_name')
+    # locales = MedianHouseholdIncome.objects.filter(year=year).filter(state_name=state_name).filter(geography_name=geography_name).distinct()
+    # context = {'locales': locales}
+    # return render(request, "partials/locales.html", context=context)
