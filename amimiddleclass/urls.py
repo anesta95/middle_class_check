a2a2a72f4348992b04d151de188c55094793d61a
anesta95 <adriannesta@gmail.com>
2023-10-10 08:39:07 -0400

    fixing url with app name and adding mysqlparser python package

--- amimiddleclass/urls.py
+++ amimiddleclass/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path("middle_class_check/", include("middle_class_check.urls")),
+    # path("middle_class_check/", include("middle_class_check.urls")),
+    path("", include("middle_class_check.urls")),
     path('admin/', admin.site.urls),
 ]
